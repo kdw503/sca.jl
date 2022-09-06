7413b037a2c3bd08d201b07869b18995f11b2ebd
Dae Woo Kim <kim503@wustl.edu>
2022-09-06 11:13:22 -0500

    fix pbyp

--- convexexpr.jl
+++ convexexpr.jl
@@ -215,6 +215,30 @@ function minpix(Mw,Mh,W0,H0,i,k,λ,β,order)
     xsol
 end
 
+function minpix(Mw,Mh,W0,H0,l,k,λ,β,order)
+    p = size(W0,2)
+    x = Variable(1)
+    # invertibility
+    mwk = Mw[:,k]; mwlk=mwk[l]; mhl = Mh[l,:]
+    Eprev = I-Mw*Mh; Eprevl = Eprev[l,:]; El = Eprevl + mwlk*mhl
+    invertibility = sumsquares(El-x*mhl)
+    # sparsity and non-negativity
+    w0l = W0[:,l]; w0mwk = W0*mwk; w0mwlk = w0mwk-w0l*mwlk; w0mwlkx = w0mwlk+w0l*x
+    sparsity = order == 1 ? norm(w0mwlkx, 1) : sumsquares(w0mwlkx)
+    nnegativity = sumsquares(max(0,-w0mwlkx))
+    # solve
+    problem = minimize(invertibility + λ*nnegativity + β*sparsity)
+    solve!(problem, SCS.Optimizer; silent_solver = true)
+    xsol = x.value
+    # errprev = norm(Eprevl)^2; err = norm(El-xsol*mhk)^2
+    # sparsprev = order == 1 ? β*norm(w0mwk,1) : β*norm(w0mwk)^2
+    # spars = order == 1 ? β*norm(w0mwlk+w0l*xsol,1) : β*norm(w0mwlk+w0l*xsol)^2
+    # nnegprev = λ*norm(min.(0,w0mwk))^2
+    # nneg = λ*norm(min.(0,w0mwlk+w0l*xsol))^2
+    # errprev+sparsprev+nnegprev < err+spars+nneg && @show problem.status
+    xsol
+end
+
 function mincol(Mw,Mh,W0,H0,k,λ,β,order)
     p = size(W0,2)
     Eprev = I-Mw*Mh
@@ -227,9 +251,9 @@ function mincol(Mw,Mh,W0,H0,k,λ,β,order)
     xsol = x.value
     errprev = norm(Eprev)^2; err = norm(E-xsol*mhk)^2
     sparsprev = order == 1 ? β*norm(W0*Mw[:,k],1) : β*norm(W0*Mw[:,k])^2
-    spars = order == 1 ? β*norm(W0*xsol,1) : β*norm(W0*xsol)^2 
+    spars = order == 1 ? β*norm(W0*xsol,1) : β*norm(W0*xsol)^2
     nnegprev = λ*norm(min.(0,W0*Mw[:,k]))^2
-    nneg = λ*norm(min.(0,W0*xsol))^2 
+    nneg = λ*norm(min.(0,W0*xsol))^2
     errprev+sparsprev+nnegprev < err+spars+nneg && @show problem.status
     xsol
 end
@@ -237,8 +261,8 @@ end
 function minMw_pixel!(Mw,Mh,W0,H0,λ,β,order)
     p = size(Mw,2)
     for k in 1:p
-        for i in 1:p
-            Mw[i,k] = minpix(Mw,Mh,W0,H0,i,k,λ,β,order)
+        for l in 1:p
+            Mw[l,k] = minpix(Mw,Mh,W0,H0,l,k,λ,β,order)
         end
     end
 end
